9f2459dfaa801f18c2c203576f5b1dffb8c9b3b6
ZHANG_Zhigang <NicoBravo@aliyun.com>
2022-09-12 19:38:21 +0200

    ex1修改

--- Experiment/ex1.m
+++ Experiment/ex1.m
@@ -3,29 +3,11 @@ clc
 close all
 yalmip('clear')
 
-blues = randn(2,25);
-greens = randn(2,25)+2;
-% plot(greens(1,:),greens(2,:),'g*')
-% hold on
-% plot(blues(1,:),blues(2,:),'b*')
-a = sdpvar(2,1);
-b = sdpvar(1);
-u = sdpvar(1,25);
-v = sdpvar(1,25);
-Constraints = [a'*greens+b >= 1-u, a'*blues+b <= -(1-v), u >= 0, v >= 0];
-Objective = sum(u)+sum(v);
-Constraints = [Constraints, -1 <= a <= 1];
-options = sdpsettings('solver','mosek')
-% options = sdpsettings(options, 'verbose',0)
-options.verbose = 0;
-options.showprogress = 1;
-options.cachesolvers = 1;
-sol=optimize(Constraints,Objective, options)
-x = sdpvar(2,1);
-P1 = [-5<=x<=5, value(a)'*x+value(b)>=0];
-P2 = [-5<=x<=5, value(a)'*x+value(b)<=0];
-% clf
-% plot(P1);hold on
-% plot(P2);
-% plot(greens(1,:),greens(2,:),'g*')
-% plot(blues(1,:),blues(2,:),'b*')
+a1=1; a2=0.5; a3=0.1;
+x = -1:0.01:1;
+y1 = -a1*x+1;
+y2 = -a2*x+1;
+y3 = -a3*x+1;
+
+plot(x,y1,x,y2,x,y3)
+legend('a1=1','a2=0.5','a3=0.1')
